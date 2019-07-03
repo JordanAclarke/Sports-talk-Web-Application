54340362fac88ce78520a2ab06c66e69af0e7438
JordanAclarke <JordanAclarke@gmail.com>
2019-07-03 15:06:49 -0400

    server changes

--- server.js
+++ server.js
@@ -65,6 +65,11 @@ app.set('view engine', 'hbs')
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
+
+app.get('/', (req, res) => {
+    res.redirect('/posts')
+})
+
 app.use('/posts', postRouter)
 app.use('/users', userRouter)
 // app.use('/users/:userId/posts', postRouter)
